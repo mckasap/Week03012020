4b533068b0f6dff5a0583ac249e02cd951a4d381
mckasap <mckasap@gmail.com>
2020-02-27 14:14:03 +0300

    Initial commit

---
+++ app/src/main/java/org/kasapbasi/week030012020/MainActivity.java
@@ -0,0 +1,14 @@
+package org.kasapbasi.week030012020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
